381d156e29b46f0dd84ab8c6f0ab2f17134b4d84
Ederson Davi <silva.edersonqueiroz@gmail.com>
2021-03-26 20:04:53 -0300

    resolution problem 17

--- src/pages/Number_letter_counts/index.tsx
+++ src/pages/Number_letter_counts/index.tsx
@@ -4,38 +4,54 @@ import Button from '../../components/Button';
 const NumberLetterCounts: React.FC = () => {
   const [result, setResult] = React.useState<number>(0)
   const stringNumbers = ['zero', 'one', 'two', 'three', "four", "five", 'six', 'seven', 'eight', 'nine']
-  const stringNumbersTeen = ['ten', 'twelve', 'eleven', 'thirteen', 'fourteen', "fifteen", "sixteen", 'seventeen', 'eighteen', 'nineteen']
-  const stringNumbersDec = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', "seventy", "eighty", 'ninety']
+  const stringNumbersTeen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', "fifteen", "sixteen", 'seventeen', 'eighteen', 'nineteen']
+  const stringNumbersDec = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', "seventy", "eighty", 'ninety']
 
   const handleCalculate = () => {
     let numberLetter = ''
-    for (let i = 1; i < 1000; i++) {
-      if (i < 10) {
-        numberLetter += stringNumbers[i]
-      } else
-        if (i > 9 && i < 20) {
+    for (let i = 1; i <= 1000; i++) {
+      if (i === 1000) {
+        numberLetter += 'onethousand'
+      } else {
+        if (i < 10) {
+          numberLetter += stringNumbers[i]
+
+        } else if (i > 9 && i < 20) {
           if (i === 11) {
-            numberLetter += stringNumbersTeen[1]
+            numberLetter += 'eleven'
           } else {
             const number = Number(i.toString().split('1')[1])
             numberLetter += stringNumbersTeen[number]
           }
 
-        } else if (i > 19 && i < 30) {
-          if (i === 20) {
-            numberLetter += stringNumbersDec[0]
-          } else if (i === 22) {
-            numberLetter += `${stringNumbersDec[0]}-${stringNumbers[2]}`
+        } else if (i < 100) {
+          if (i.toString().includes('0')) {
+            numberLetter += stringNumbersDec[Number(i.toString()[0])]
           } else {
-            const number = Number(i.toString().split('2')[1])
-            console.log(number, i);
-
-            numberLetter += `${stringNumbersDec[0]}-${stringNumbers[number]}`
+            numberLetter += `${stringNumbersDec[Number(i.toString()[0])]}${stringNumbers[Number(i.toString()[1])]}`
           }
+        } else {
+          if (i.toString().includes('00')) {
+            numberLetter += `${stringNumbers[Number(i.toString()[0])]}hundred`
+          } else
+            if (Number(i.toString()[1]) === 0) {
+              numberLetter += `${stringNumbers[Number(i.toString()[0])]}hundredand${stringNumbers[Number(i.toString()[2])]}`
+            } else if (i.toString().includes('10')) {
+              numberLetter += `${stringNumbers[Number(i.toString()[0])]}hundredand${stringNumbersTeen[Number(i.toString()[2])]}`
+            } else if (Number(i.toString()[1]) === 1 && Number(i.toString()[2]) > 0) {
+              numberLetter += `${stringNumbers[Number(i.toString()[0])]}hundredand${stringNumbersTeen[Number(i.toString()[2])]}`
+            } else if (Number(i.toString()[1]) >= 2) {
+              if (i.toString()[2] === '0') {
+                numberLetter += `${stringNumbers[Number(i.toString()[0])]}hundredand${stringNumbersDec[Number(i.toString()[1])]}`
+              } else {
+                numberLetter += `${stringNumbers[Number(i.toString()[0])]}hundredand${stringNumbersDec[Number(i.toString()[1])]}${stringNumbers[Number(i.toString()[2])]}`
+              }
+            }
         }
-    }
-    console.log(numberLetter);
 
+      }
+    }
+    setResult(numberLetter.length);
   }
 
   return (
